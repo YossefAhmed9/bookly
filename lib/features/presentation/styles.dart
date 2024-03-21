0f671a214a38b4ac915ea807e0ad1dc83c802f27
Yossef Ahmed <yossefahmed209@gmail.com>
2024-03-22 01:14:06 +0200

    adding GT-Sectra Font Family

--- lib/features/presentation/styles.dart
+++ lib/features/presentation/styles.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 
-class Styles  {
+class Styles {
+  TextStyle? title1 = const TextStyle(
+      fontWeight: FontWeight.w600, fontSize: 25, fontFamily: 'Sectra');
 
-  TextStyle? title1= const TextStyle(
-      fontWeight: FontWeight.w600,fontSize: 18,fontFamily: 'Raleway'
+  TextStyle? bookTitle = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 22,
+    fontFamily: 'Raleway',
   );
 
-  TextStyle? bookTitle= const TextStyle(
-      fontWeight: FontWeight.w600,fontSize: 22,fontFamily: 'Raleway',
-  );
-
-
-  TextStyle? authorName= const TextStyle(
-      fontSize: 15,fontFamily: 'Raleway',color: Colors.grey
-  );
+  TextStyle? authorName =
+      const TextStyle(fontSize: 15, fontFamily: 'Sectra', color: Colors.grey);
 
   TextStyle? rating = const TextStyle(
-      fontWeight: FontWeight.w300,fontSize: 18,fontFamily: 'Raleway'
-  );
+      fontWeight: FontWeight.w300, fontSize: 18, fontFamily: 'Sectra');
 
   TextStyle? price = const TextStyle(
-      fontWeight: FontWeight.w600,fontSize: 18,fontFamily: 'Raleway'
-  );
-  }
-
+      fontWeight: FontWeight.w600, fontSize: 18, fontFamily: 'Sectra');
+}
