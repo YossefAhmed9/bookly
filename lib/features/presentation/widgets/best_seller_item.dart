9558393462e361295b351fff058e3a42a52da0a8
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-06-30 09:08:05 +0300

    Adding height parameter

--- lib/features/presentation/widgets/best_seller_item.dart
+++ lib/features/presentation/widgets/best_seller_item.dart
@@ -3,79 +3,88 @@ import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/data/models/book_model.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerItem extends StatelessWidget {
-  const BestSellerItem({super.key, required this.bestSellerIndex});
-  final int bestSellerIndex;
+  BestSellerItem({super.key, required this.bestSellerIndex});
+  int bestSellerIndex;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(),
-      child: Row(
-        children: [
-          Container(
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            width: 100,
-            height: MediaQuery.of(context).size.height * 0.15,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Image(
-              image: NetworkImage(
-                BookModel.fromJson(
-                        json: BooklyCubit.get(context).allBooksResult,
-                        index: bestSellerIndex)
-                    .normalThumbnail,
+    return GestureDetector(
+      onTap: () {
+        //bestSellerIndex=BooklyCubit.get(context).bookIndex;
+        BooklyCubit.get(context).bookIndex = bestSellerIndex;
+
+        GoRouter.of(context).push('/bookDetails');
+      },
+      child: Container(
+        decoration: const BoxDecoration(),
+        child: Row(
+          children: [
+            Container(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              width: 100,
+              height: MediaQuery.of(context).size.height * 0.15,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Image(
+                image: NetworkImage(
+                  BookModel.fromJson(
+                          json: BooklyCubit.get(context).allBooksResult,
+                          index: bestSellerIndex)
+                      .normalThumbnail,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  child: Text(
-                    BookModel.fromJson(
-                            json: BooklyCubit.get(context).allBooksResult,
-                            index: bestSellerIndex)
-                        .title
-                        .toString(),
-                    maxLines: 2,
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.bookTitle,
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: Text(
+                      BookModel.fromJson(
+                              json: BooklyCubit.get(context).allBooksResult,
+                              index: bestSellerIndex)
+                          .title
+                          .toString(),
+                      maxLines: 2,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.bookTitle,
+                    ),
                   ),
-                ),
-                Text(
-                    '${BookModel.fromJson(json: BooklyCubit.get(context).allBooksResult, index: bestSellerIndex).authors[0].toString()}',
-                    style: Styles.authorName),
-                Row(
-                  children: [
-                    Text('19.99\$', style: Styles.price),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.17,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(right: 8.0),
-                      child: Icon(
-                        FontAwesomeIcons.solidStar,
-                        color: Colors.amberAccent,
+                  Text(
+                      BookModel.fromJson(json: BooklyCubit.get(context).allBooksResult, index: bestSellerIndex).authors[0].toString(),
+                      style: Styles.authorName),
+                  Row(
+                    children: [
+                      Text('19.99\$', style: Styles.price),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.17,
                       ),
-                    ),
-                    const Text('4.8'),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    const Text('(2389)'),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+                      const Padding(
+                        padding: EdgeInsets.only(right: 8.0),
+                        child: Icon(
+                          FontAwesomeIcons.solidStar,
+                          color: Colors.amberAccent,
+                        ),
+                      ),
+                      const Text('4.8'),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text('(2389)'),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/features/presentation/widgets/best_seller_item_listView.dart
+++ lib/features/presentation/widgets/best_seller_item_listView.dart
@@ -1,43 +1,25 @@
 import 'package:bookly/core/cubit/home_cubit/cubit.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import '../../../core/utils/styles.dart';
 import 'Best_seller_item.dart';
 
 class BestSellerItemListView extends StatelessWidget {
-  const BestSellerItemListView({super.key,required this.height});
-final double height;
+  const BestSellerItemListView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-       const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: Text(
-            'Best Sellers',
-            style: Styles.title1,
-          ),
-        ),
-        SizedBox(
+    return SizedBox(
+      width: double.infinity,
+      child: ListView.builder(
+         itemCount: BooklyCubit.get(context).allBooksItems.length,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) =>  SizedBox(
           width: double.infinity,
-          child: Expanded(
-            child: ListView.builder(
-               itemCount: BooklyCubit.get(context).allBooksItems.length,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) =>   SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0),
-                  child: BestSellerItem(bestSellerIndex: index,),
-                ),
-              ),
-            ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: BestSellerItem(bestSellerIndex: index,),
           ),
         ),
-      ],
+      ),
     );
   }
 }
