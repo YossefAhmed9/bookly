424452b46ca7a68527cc752ef4f2879adaf3bf1a
Yossef Ahmed Abd El Raheem <yossefahmed209@gmail.com>
2024-06-30 09:09:51 +0300

    Editing the item itself to be make it identical AP

--- lib/features/presentation/widgets/book_item.dart
+++ lib/features/presentation/widgets/book_item.dart
@@ -1,7 +1,6 @@
 import 'package:bookly/core/cubit/home_cubit/cubit.dart';
 import 'package:bookly/data/models/book_model.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class BookItem extends StatelessWidget {
   const BookItem({super.key,required this.bookItemIndex});
@@ -14,6 +13,7 @@ final int bookItemIndex;
         clipBehavior: Clip.antiAliasWithSaveLayer,
         decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
         child:  Image(
+          fit: BoxFit.fill,
           image: NetworkImage(BookModel.fromJson(json: BooklyCubit.get(context).allBooksResult,index: bookItemIndex).smallThumbnail)
         ),
       ),
